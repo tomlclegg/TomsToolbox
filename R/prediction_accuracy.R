ac8cabc9036ad7b97f59cf48e86add2d98a2e652
Clegg <a5587@hi.no>
2021-07-08 08:58:29 +0200

    first commit

---
+++ R/prediction_accuracy.R
@@ -0,0 +1,15 @@
+#' Prediction accuracy
+#'
+#' Various measures of prediction accuracy
+#' @param pred a numeric vector of predictions
+#' @param obs a numeric vector of observations
+#' @details
+#' rmse: root mean square error
+#' rrmse: relative root mean square error
+#' mpe: mean percentage error
+#' @export
+rmse <- function(pred, obs) sqrt(mean((pred - obs)^2))
+
+rrmse <- function(pred, obs) sqrt(mean((pred - obs)^2)) / mean(obs)
+
+mpe <- function(pred, obs) mean((pred - obs) / pred) * 100
